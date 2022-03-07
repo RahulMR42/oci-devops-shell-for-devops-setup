b441ea717e3ecd577a85ef8007d2c183267ed9e9
Rahul M R <rahul.m.r@oracle.com>
2022-03-07 18:11:30 +0530

    trial

--- oci-devops.sh
+++ oci-devops.sh
@@ -6,6 +6,6 @@
 
 UUID=`uuidgen|cut -f 4 -d '-'`
 OCIPROJECT="devops-prj-${UUID}"
-read -e -i "$OCIPROJECT" -p "Enter Project Name " input
+read -e -i "(DEFAULT : $OCIPROJECT)" -p " Enter Project Name " input
 OCIPROJECT="${input:-$OCIPROJECT}"
 echo "${OCIPROJECT}"
