9ac983438fdfd9ca9f37cacae1e62dd596c94b43
Rahul M R <rahul.m.r@oracle.com>
2022-03-07 19:56:33 +0530

    trial

--- oci-devops.sh
+++ oci-devops.sh
@@ -7,11 +7,7 @@
 # UUID=`uuidgen|cut -f 4 -d '-'`
 # OCIPROJECT="devopsprj-${UUID}"
 
-# echo "Here is the available compartments"
 
-# oci iam compartment  list --query "data[*].{Name:name,ID:id}" --output table
-
-# read -p "OCI Compartment OCID ?:" compartmentid
 
 # echo "Here are the available OCI notification topics"
 
@@ -48,7 +44,7 @@ ColorBlue(){
 
 main_menu(){
 echo -ne "
-OCI Devops Quick menu
+OCI Devops Quick menu - Compartment ID : $1
 $(ColorGreen '1)') devops project
 $(ColorGreen '2)') build pipeline
 $(ColorGreen '3)') deploy pipeline
@@ -68,5 +64,7 @@ $(ColorBlue 'Choose an option:') "
         esac
 }
 
-# Call the menu function
-main_menu
+echo "Here is the available compartments"
+oci iam compartment  list --query "data[*].{Name:name,ID:id}" --output table
+read -p "OCI Compartment OCID ?:" compartmentid
+main_menu(compartmentid)
