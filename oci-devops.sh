0e9d6b33f3e4c058bccbee575ae5deca3846f19d
Rahul M R <rahul.m.r@oracle.com>
2022-03-07 19:51:39 +0530

    trial

--- oci-devops.sh
+++ oci-devops.sh
@@ -26,53 +26,7 @@
 
 #!/bin/bash
 
-##
-# BASH menu script that checks:
-#   - Memory usage
-#   - CPU load
-#   - Number of TCP connections 
-#   - Kernel version
-##
 
-server_name=$(hostname)
-
-function memory_check() {
-    echo ""
-	echo "Memory usage on ${server_name} is: "
-	free -h
-	echo ""
-}
-
-function cpu_check() {
-    echo ""
-	echo "CPU load on ${server_name} is: "
-    echo ""
-	uptime
-    echo ""
-}
-
-function tcp_check() {
-    echo ""
-	echo "TCP connections on ${server_name}: "
-    echo ""
-	cat  /proc/net/tcp | wc -l
-    echo ""
-}
-
-function kernel_check() {
-    echo ""
-	echo "Kernel version on ${server_name} is: "
-	echo ""
-	uname -r
-    echo ""
-}
-
-function all_checks() {
-	memory_check
-	cpu_check
-	tcp_check
-	kernel_check
-}
 
 ##
 # Color  Variables
@@ -94,20 +48,20 @@ ColorBlue(){
 
 menu(){
 echo -ne "
-My First Menu
-$(ColorGreen '1)') Memory usage
-$(ColorGreen '2)') CPU load
-$(ColorGreen '3)') Number of TCP connections 
-$(ColorGreen '4)') Kernel version
-$(ColorGreen '5)') Check All
+OCI Devops Quick menu
+$(ColorGreen '1)') devops project
+$(ColorGreen '2)') build pipeline
+$(ColorGreen '3)') deploy pipeline
+$(ColorGreen '4)') policies & dynamic groups
+$(ColorGreen '5)') A CICD pipeline
 $(ColorGreen '0)') Exit
 $(ColorBlue 'Choose an option:') "
         read a
         case $a in
-	        1) memory_check ; menu ;;
-	        2) cpu_check ; menu ;;
-	        3) tcp_check ; menu ;;
-	        4) kernel_check ; menu ;;
+	        1) oci_prj ; menu ;;
+	        2) oci_build ; menu ;;
+	        3) oci_deploy ; menu ;;
+	        4) oci_policies ; menu ;;
 	        5) all_checks ; menu ;;
 		0) exit 0 ;;
 		*) echo -e "Invalid"
