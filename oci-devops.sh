2ad123f3ecbb9c057d8143cefb1ca703c0b9c713
Rahul M R <rahul.m.r@oracle.com>
2022-03-09 22:04:16 +0530

    adding build

--- oci-devops.sh
+++ oci-devops.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
-
-
-
-# #Variables 
-
-# UUID=`uuidgen|cut -f 4 -d '-'`
-# OCIPROJECT="devopsprj-${UUID}"
-
-
-
-# echo "Here are the available OCI notification topics"
-
-# oci ons topic list --compartment-id ${compartmentid} --all --output table --query 'data[*].{Name:name,TopicID:"topic-id"}'
-
-# read -p "OCI Notification TopicID ?:" onstopicid
-
-# read -p "Devops Project Name ? [${OCIPROJECT}]: " prjname
-# prjname=${prjname:-${OCIPROJECT}}
-
-# oci devops project create --compartment-id ${compartmentid} --name ${prjname} --notification-config '{"topicId":"'${onstopicid}'"}' --description "Project ${prjname}"
-
-#!/bin/bash
-
-
+#Shell script covering OCI CLIs for devops project.
 
 ##
 # Color  Variables
@@ -82,6 +59,30 @@ oci_prj_menu(){
 
 }
 
+oci_build_menu(){
+
+    echo "Here is the available compartments"
+    oci iam compartment  list --query "data[*].{Name:name,ID:id}" --output table
+    read -p "OCI Compartment OCID ?:" compartmentid
+    OCI Devops Build menu - ${compartmentid}    
+    echo -ne "
+    OCI Devops PROJECT menu - Compartment ID : $1
+    $(ColorGreen '1)') List build pipelines.
+    $(ColorGreen '2)') Create build pipelines.
+    $(ColorGreen '100)') Back to main.
+    $(ColorGreen '0)') exit.
+    $(ColorBlue 'Choose an option:') "
+            read choice
+            case $choice in
+                1) list_buildpipelines $compartmentid ; oci_prj_menu ;;
+                2) create_project $compartmentid;  oci_prj_menu;;
+                100) main_menu ;;
+            0) exit 0 ;;
+            *) echo -e "Invalid"
+            esac
+
+}
+
 main_menu(){
 
 
@@ -97,7 +98,7 @@ $(ColorBlue 'Choose an option:') "
         read choice
         case $choice in
                 1) oci_prj_menu ; main_menu ;;
-                2) oci_build ; main_menu ;;
+                2) oci_build_menu ; main_menu ;;
                 3) oci_deploy ; main_menu ;;
                 4) oci_policies ; main_menu ;;
                 5) all_checks ; main_menu ;;
